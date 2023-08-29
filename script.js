064f30d28167d0126789d9f10937a129b37e7cac
Artem Makarov <52400119+makarbass@users.noreply.github.com>
2023-08-29 23:10:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-var card = document.querySelector('.card');
-var backFace = card.querySelector('.card__back--in');
-var backAdd = card.querySelector('.card__back--comment')
+let card = document.querySelector('.card');
+let frontFace = card.querySelector('.card__text')
+let backFace = card.querySelector('.card__back--in');
+let backAdd = card.querySelector('.card__back--comment')
+
+const dictionary = {
+    'Какими бывают типы данных?':{
+        "face":"В Python типы данных можно разделить на две группы: изменяемые и неизменяемые",
+        "add": `Неизменяемые:<br>
+            - int<br>
+            - float<br>
+            - string<br>
+            - bool<br>
+            - NoneType<br>
+            - tuple (кортежи)<br>
+            - frozenset<br>
+        
+            Изменяемые:<br>
+            - list<br>
+            - dict<br>
+            - set (множества)`
+    },
+  'S в SOLID': {
+    "face": 'Принцип единственной ответственности',
+    "add": 'Для каждого класса должно быть определено единственное назначение. Не должно возникать God object, который занимается всем в программе.',
+    },
+  'O в SOLID': {
+    "face":'Принцип открытости/закрытости',
+    "add": '«программные сущности … должны быть открыты для расширения, но закрыты для модификации». Мы должны иметь возможность добавлять функциональность.',
+    },
+  'L в SOLID': {
+    "face": 'Принцип подстановки Барбары Лисков',
+    "add": '«объекты в программе должны быть заменяемыми на экземпляры их подтипов без изменения правильности выполнения программы». У объекта есть тип, но сами классы выстраиваются в иерархию классов. Подтип типа всю старую функциональность должен выполнять.'
+  },
+};
+
+let randomKey = Object.keys(dictionary)[Math.floor(Math.random() * Object.keys(dictionary).length)];
+frontFace.textContent = randomKey;
 
 card.addEventListener('click', function() {
   card.classList.toggle('is-flipped');
   if (card.classList.contains('is-flipped')) {
-    backFace.innerHTML = `В Python типы данных можно разделить на две группы: изменяемые и неизменяемые`;
-    backAdd.innerHTML = `Неизменяемые:<br>
-    - int<br>
-    - float<br>
-    - string<br>
-    - bool<br>
-    - NoneType<br>
-    - tuple<br>
-    - frozenset<br>
-
-    Изменяемые:<br>
-    - list<br>
-    - dict<br>
-    - set`
-  }
+    backFace.innerHTML = dictionary[randomKey]["face"];
+    backAdd.innerHTML = dictionary[randomKey]["add"]
+  } else {
+        randomKey = Object.keys(dictionary)[Math.floor(Math.random() * Object.keys(dictionary).length)];
+        frontFace.textContent = randomKey;
+    }
 });
